450c7a330ad63542125f7994aafc24c6a5e8f8ea
Kurtney <kurtjantjies18@gmail.com>
2025-02-12 14:16:42 +0200

    init: created backend project using spring starter

---
+++ src/main/java/com/kurtneyjantjies/real_estate_data_integration/RealEstateDataIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.kurtneyjantjies.real_estate_data_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateDataIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateDataIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurtneyjantjies/real_estate_data_integration/RealEstateDataIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurtneyjantjies.real_estate_data_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateDataIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
